2ae952f7fc1cb6b357fc609183f9fd47524fb30e
MartinsGabrielC <martins.gabriel.c@gmail.com>
2016-11-25 11:47:55 -0200

    Alterado para receber ID como argumento

--- RestUtil.py
+++ RestUtil.py
@@ -1,12 +1,16 @@
 from urllib.request import urlopen
 import json
-import pprint
+import sys
 
 def opportunitiesCount(ID):
     url = "http://ec2-35-164-139-210.us-west-2.compute.amazonaws.com/hirers/"+ID+"/opportunities"
     html = urlopen(url).read().decode("utf-8")
     j_obj = json.loads(html)
     print(len(j_obj))
-
-ID = input("Entre o ID que gostaria de ser verificado:")
-opportunitiesCount(ID)
+    
+try:
+	ID = sys.argv[1]
+	opportunitiesCount(ID)
+except IndexError:
+	print("Error: Enviar ID como argumento")
+	sys.exit()
