d5eb6e22321c0a34b3486f0bec7664c9b4415d57
MartinsGabrielC <martins.gabriel.c@gmail.com>
2016-12-03 18:57:58 -0200

    Criação de funções para tratar de entradas

--- Dinamica2.py
+++ Dinamica2.py
@@ -3,10 +3,69 @@ import requests
 import datetime
 import sys
 
+#inputToInt
+#input: String prompt
+#output: Integer data
+#Pede por uma entrada de dado:
+#   caso essa entrada seja vazia retorna 0
+#   senão tenta fazer a conversão da entrada para inteiro
+#       caso consigo fazer a conversão retorna o valor
+#       senão imprime mensagem de erro e pede uma nova entrada
+def inputToInt(prompt):
+    while True:
+        data = input(prompt)
+        if(not data):
+            return 0
+        else:
+            try:
+                return int(data)
+            except ValueError:
+                print("Esperado um valor inteiro")
+                continue
+
+#inputToFloat
+#input: String prompt
+#output: Float data
+#Pede por uma entrada de dado:
+#   caso essa entrada seja vazia retorna 0
+#   senão tenta fazer a conversão da entrada para real
+#       caso consigo fazer a conversão retorna o valor
+#       senão imprime mensagem de erro e pede uma nova entrada
+def inputToFloat(prompt):
+    while True:
+        data = input(prompt)
+        if(not data):
+            return 0.0
+        else:
+            try:
+                return float(data)
+            except ValueError:
+                print("Esperado um valor real")
+                continue
+
+#inputToBool
+#input: String prompt
+#output: Bool data
+#Pede por uma entrada de dado:
+#   caso essa entrada seja 'y' ou 'Y' retorna verdadeiro
+#   caso essa entrada seja vazia, 'n' ou 'N' retorna falso
+#   senão imprime mensagem de erro e pede uma nova entrada
+def inputToBool(prompt):
+    while True:
+        data = input(prompt).lower()
+        if data == 'y':
+            return True
+        elif data == 'n' or not data:
+            return False
+        else:
+            print("Esperado y ou n")
+            continue
+
 def postOpportunitie():
     #Url a ser enviado os dados
     url = "http://ec2-35-164-223-211.us-west-2.compute.amazonaws.com/opportunities"
     #ciacao do dicionario que recebera todos os dados a serem enviados
+    #dados são colocados na formatação de json
     data = {}
     #dados do contratante sao definidos fixo
     data['hirer']= {
@@ -21,53 +80,56 @@ def postOpportunitie():
         "is_plan_active":True
     }
     #inicio da leitura dos dados que serao enviados
-    data['id']=int(str(data['hirer']['id'])+input("id ")) #id e composto do id do contratante e do id da vaga definida
+    oppID = inputToInt("id ")
+    data['id']=int(str(data['hirer']['id']) + str(abs(oppID))) #id e composto do id do contratante e do id da vaga definida
     data['title']=input("title ")
     data['description']=input("description ")
-    data['created_at']=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") #data de criacao e definida automaticamente a partir do horario atual do host do script
-    data['is_contact_available']=True if(input("is_contact_available(y/n) ").lower()=="y") else False  #caso a entrada seja 'y' ou 'Y' envia True, qualquer outra coisa retorna False
-    data['is_active']=True if(input("is_active?(y/n) ").lower()=="y") else False #caso a entrada seja 'y' ou 'Y' envia True, qualquer outra coisa retorna False
+    data['created_at']=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") #data de criacao e definida automaticamente
+    data['is_contact_available']=inputToBool("is_contact_available(y/n) ")
+    data['is_active']=inputToBool("is_active?(y/n) ")
 
     data['location']={} #inicializacao do dicionario vazio para receber os dados de localizacao
     data['location']['neighborhood']=input("neighborhood ")
     data['location']['address']=input("address ")
     data['location']['address_type']=input("address_type ")
-    data['location']['latitude']=float(input("latitude ") or 0) #para caso de entrada vazia o valor default sera o '0'
-    data['location']['longitude']=float(input("longitude ") or 0) #para caso de entrada vazia o valor default sera o '0'
+    data['location']['latitude']=inputToFloat("latitude ")
+    data['location']['longitude']=inputToFloat("longitude ")
     data['location']['city_id']=input("city_id ")
     data['location']['city']=input("city ")
     data['location']['zipcode']=input("zipcode ")
     data['location']['state']=input("state ")
 
     data['frequency']=input("frequency ")
-    data['is_automatic']=True if(input("is_automatic(y/n) ").lower()=="y") else False #caso a entrada seja 'y' ou 'Y' envia True, qualquer outra coisa retorna False
-    data['score']=int(input("score ") or 0) #para caso de entrada vazia o valor default sera o '0'
+    data['is_automatic']=inputToBool("is_automatic(y/n) ")
+    data['score']=inputToInt("score ")
 
     data['category']={}
-    data['category']['id']=int(input("category id ") or 0) #para caso de entrada vazia o valor default sera o '0'
+    data['category']['id']=inputToInt("category id ")
     data['category']['name']=input("category name ")
 
-    data['salary_requirements']=int(input("salary_requirements ") or 0) #para caso de entrada vazia o valor default sera o '0'
+    data['salary_requirements']=inputToInt("salary_requirements ")
     data['characteristics']=[]
     data['starts']=input("starts ")
-    data['amout_candidates']=int(input("amout_candidates ") or 0) #para caso de entrada vazia o valor default sera o '0'
-    data['amount_visualizations']=int(input("amount_visualizations ") or 0) #para caso de entrada vazia o valor default sera o '0'
+    data['amout_candidates']=inputToInt("amout_candidates ")
+    data['amount_visualizations']=inputToInt("amount_visualizations ")
     data['feedback']=input("feedback ")
     data['salary_research']=input("salary_research ")
     data['relevancy']=input("relevancy ")
 
-    #envio dos dados para o servidor
     try:
+        #tentativa de enviar os dados para o servidor
         html = requests.post(url,json=data)
     except :
-        print("Erro: Servidor não pode ser acessado")
+        #caso o servidor não esteja acessível
+        print("\nErro: Servidor não pode ser acessado")
         sys.exit()
-    #caso o envio nao tenha retornado erros
+
     if(html.ok):
-        print("Vaga Adicionada")
-    #em caso de erro imprime o codigo do erro e o texto referente a este erro
+        #caso o envio nao tenha retornado erros
+        print("\nVaga Adicionada")
     else:
-        print("Erro Status:" +html.status_code)
+        #em caso de erro imprime o codigo do erro e o texto referente a este erro
+        print("\nErro Status:" +html.status_code)
         print(html.json()['errors'])
 
 postOpportunitie()
